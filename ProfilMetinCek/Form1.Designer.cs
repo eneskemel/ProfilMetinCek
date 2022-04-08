aac7fc13b065461fda4dcb7c866397d9935a882d
Enes <eneskemel@gmail.com>
2022-04-08 09:52:18 +0300

    Add project files.

---
+++ ProfilMetinCek/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ProfilMetinCek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.AsagiKaydirTmr = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(351, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(993, 691);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(333, 381);
+            this.listBox1.TabIndex = 2;
+            // 
+            // AsagiKaydirTmr
+            // 
+            this.AsagiKaydirTmr.Interval = 1000;
+            this.AsagiKaydirTmr.Tick += new System.EventHandler(this.AsagiKaydirTmr_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "islemSaniyesi";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1356, 715);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer AsagiKaydirTmr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ProfilMetinCek/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.ComponentModel;
+using System.Collections;
+using Microsoft.Win32;
+
+namespace ProfilMetinCek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static SqlConnection baglanti = new SqlConnection("Data Source = 185.141.33.104\\SQLEXPRESS;" + "Initial Catalog = facebook;" + "User id = sa;" + "Password=1236asd;");
+        System.Collections.ArrayList arr1 = new System.Collections.ArrayList(); // listbox1
+        System.Collections.ArrayList arr2 = new System.Collections.ArrayList(); // listbox2
+        System.Collections.ArrayList arr3 = new System.Collections.ArrayList(); // listbox2
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string kayit = "insert into Gonderiler(gonderiIcerik,gonderiTip) values (@gonderiIcerik, @gonderiTip)";
+                SqlCommand komutpaylasim = new SqlCommand(kayit, baglanti);
+                komutpaylasim.Parameters.AddWithValue("@gonderiIcerik", listBox1.Items[i].ToString());
+                komutpaylasim.Parameters.AddWithValue("@gonderiTip", "0");
+                komutpaylasim.ExecuteNonQuery();
+
+            }
+            baglanti.Close();
+            // AsagiKaydirTmr.Start();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("");
+        }
+        int islemSaniyesi = 0;
+        private void AsagiKaydirTmr_Tick(object sender, EventArgs e)
+        {
+            islemSaniyesi++;
+            label1.Text = islemSaniyesi.ToString();
+            if (islemSaniyesi < 100)
+            {
+                webBrowser1.Document.Window.ScrollTo(0, webBrowser1.Document.Body.ScrollRectangle.Height);
+            }
+            if (islemSaniyesi == 105)
+            {
+                string html = webBrowser1.Document.Body.InnerHtml.ToString();
+                string[] parcalar;
+                parcalar = html.Split('<');
+                arr1.Clear();
+                foreach (string i in parcalar)
+                {
+                    arr1.Add(i);
+                }
+                arr2.Clear();
+                for (int i = 0; i < arr1.Count; i++)
+                {
+                    string Metin = arr1[i].ToString();
+                    int sonuc = Metin.IndexOf("_5pbx userContent _22jv _3576");
+                    if (sonuc > 0)
+                    {
+                        string sayfa = (arr1[i + 2].ToString());
+                        listBox1.Items.Add(sayfa.Remove(0, 2));
+                    }
+                }
+            }
+            if (islemSaniyesi == 110)
+            {
+                string yol = "F:\\Durumlar.txt";
+                StreamWriter s = new StreamWriter(yol);
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    s.WriteLine(listBox1.Items[i].ToString());
+                }
+                s.Close();
+                islemSaniyesi = 0;
+                AsagiKaydirTmr.Stop();
+                //baglanti.Open();
+                //for (int i = 0; i < listBox1.Items.Count; i++)
+                //{
+                //    string kayit = "insert into Gezinti(GezintiList) values (@GezintiList)";
+                //    SqlCommand komutpaylasim = new SqlCommand(kayit, baglanti);
+                //    komutpaylasim.Parameters.AddWithValue("@GezintiList", listBox1.Items[i].ToString());
+                //    komutpaylasim.ExecuteNonQuery();
+
+                //}
+                //baglanti.Close();
+            }
+        }
+        void SayfaAdresiCek()
+        {
+            string html = webBrowser1.Document.Body.InnerHtml.ToString();
+            string[] parcalar;
+            parcalar = html.Split('<');
+            arr1.Clear();
+            foreach (string i in parcalar)
+            {
+                arr1.Add(i);
+            }
+            arr2.Clear();
+            for (int i = 0; i < arr1.Count; i++)
+            {
+                string Metin = arr1[i].ToString();
+                int sonuc = Metin.IndexOf("_4-u3 _5l2c");
+                if (sonuc > 0)
+                {
+                    string sayfa = (arr1[i + 2].ToString().Remove(0, 34));
+                    string[] parcalar2;
+                    parcalar2 = sayfa.Split('"');
+                    arr3.Clear();
+                    foreach (string i2 in parcalar2)
+                    {
+                        arr3.Add(i2);
+                    }
+                    listBox1.Items.Add(arr3[0].ToString());
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string yol = "F:\\Durumlar.txt";
+            StreamReader oku;
+            oku = File.OpenText(yol);
+            string yazi;
+            while ((yazi = oku.ReadLine()) != null)
+            {
+                listBox1.Items.Add(yazi.ToString());
+            }
+            oku.Close();
+        }
+    }
+}
